ff822bd2e46724b914222941e922a5273ffb4cb9
Nikola Bijelic <nikolabijelic94@gmail.com>
2017-07-11 12:02:39 +0200

    finito

--- Asynk/Asynk/Form1.Designer.cs
+++ Asynk/Asynk/Form1.Designer.cs
@@ -28,20 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnOnePage = new System.Windows.Forms.Button();
+            this.btnManyPages = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnOnePage
+            // 
+            this.btnOnePage.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnOnePage.Font = new System.Drawing.Font("Consolas", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOnePage.Location = new System.Drawing.Point(128, 269);
+            this.btnOnePage.Name = "btnOnePage";
+            this.btnOnePage.Size = new System.Drawing.Size(431, 144);
+            this.btnOnePage.TabIndex = 0;
+            this.btnOnePage.Text = "Load One Page";
+            this.btnOnePage.UseVisualStyleBackColor = false;
+            this.btnOnePage.Click += new System.EventHandler(this.btnOnePage_Click);
+            // 
+            // btnManyPages
+            // 
+            this.btnManyPages.BackColor = System.Drawing.Color.Red;
+            this.btnManyPages.Font = new System.Drawing.Font("Consolas", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnManyPages.Location = new System.Drawing.Point(128, 429);
+            this.btnManyPages.Name = "btnManyPages";
+            this.btnManyPages.Size = new System.Drawing.Size(431, 136);
+            this.btnManyPages.TabIndex = 1;
+            this.btnManyPages.Text = "Load Many Pages";
+            this.btnManyPages.UseVisualStyleBackColor = false;
+            this.btnManyPages.Click += new System.EventHandler(this.btnManyPages_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(692, 353);
+            this.BackColor = System.Drawing.Color.DarkKhaki;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(708, 773);
+            this.Controls.Add(this.btnManyPages);
+            this.Controls.Add(this.btnOnePage);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Hejhej";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnOnePage;
+        private System.Windows.Forms.Button btnManyPages;
     }
 }
 
--- Asynk/Asynk/Form1.cs
+++ Asynk/Asynk/Form1.cs
@@ -7,14 +7,68 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Http;
+using System.Diagnostics;
+
 
 namespace Asynk
 {
     public partial class Form1 : Form
     {
+        const string url = "http://www.aftonbladet.se/";
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnOnePage_Click(object sender, EventArgs e)
+        {
+            Task.Run(async () =>
+            {
+                await DownloadOneWebsite();
+            });
+        }
+
+        private async Task DownloadOneWebsite()
+        {
+            var client = new HttpClient();
+            var timer = new Stopwatch();
+            timer.Start();
+            string result = null;
+
+            result = await client.GetStringAsync(url);
+
+            timer.Stop();
+            MessageBox.Show($"www.Aftonbladet.se är nedladdad på {timer.Elapsed.TotalSeconds.ToString()} sekunder");
+
+        }
+
+        private void btnManyPages_Click(object sender, EventArgs e)
+        {
+            Task.Run(async () =>
+            {
+                await DownloadManyWebsites();
+            });
+
+
+        }
+
+        private async Task DownloadManyWebsites()
+        {
+            var client = new HttpClient();
+            var timer = new Stopwatch();
+            timer.Start();
+
+            var task1 = client.GetStringAsync(url);
+            var task2 = client.GetStringAsync(url);
+            var task3 = client.GetStringAsync(url);
+            var task4 = client.GetStringAsync(url);
+
+            string[] results = await Task.WhenAll(task1, task2, task3, task4);
+
+            timer.Stop();
+            MessageBox.Show($"www.Aftonbladet.se är nedladdat 4 gånger på {timer.Elapsed.TotalSeconds.ToString()} sekunder");
+
         }
     }
 }
